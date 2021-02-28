27edfad123de54ac14ea87135314ed64f1f194cc
jeongoo <jjunjeong@naver.com>
2021-03-01 00:56:44 +0900

    so...sexy....Wow~~

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+function Food({fav}) {
+  return <h1>I love {fav}</h1>
+}
+
 function App() {
-  return <div>hello</div> ; 
+  return ( 
+  <div>
+    <h1>http://192.168.35.96:3000</h1>
+    <Food fav="a"/>
+    <Food fav="b"/>
+    <Food fav="c"/>
+    <Food fav="d"/>
+    <Food fav="e"/>
+    <Food fav="f"/>
+    <Food fav="g"/>
+    </div>
+  ); 
 }
 
 export default App;
